e601b57893c6d3b4b481c95d256e52586fcbb51b
Michelle Victoriano <desenvolvimento12@sifat.com.br>
2023-09-28 11:47:19 -0300

    Created CreditService

---
+++ src/main/kotlin/me/dio/credit/application/system/service/impl/CreditService.kt
@@ -0,0 +1,33 @@
+package me.dio.credit.application.system.service.impl
+
+import me.dio.credit.application.system.model.entity.Credit
+import me.dio.credit.application.system.repository.CreditRepository
+import me.dio.credit.application.system.service.ICreditService
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+class CreditService(
+    private val creditRepository: CreditRepository,
+    private val customerService: CustomerService
+) : ICreditService {
+
+    override fun save(credit: Credit): Credit {
+        credit.apply {
+            customer = customerService.findById(credit.customer?.id!!)
+        }
+        return creditRepository.save(credit)
+    }
+
+    override fun findAllByCustomer(customerId: Long): List<Credit> =
+        this.creditRepository.findAllByCustomerId(customerId)
+
+
+    override fun findByCreditCode(customerId: Long, creditCode: UUID): Credit {
+        val credit: Credit = this.creditRepository.findByCreditCode(creditCode)
+            ?: throw RuntimeException("Credit $creditCode not found")
+
+        return if (credit.customer?.id == customerId) credit else throw RuntimeException("Contact Admin")
+    }
+
+}
